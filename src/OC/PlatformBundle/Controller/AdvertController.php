204db701f09ced1de6daee10a0ca57c4cd6db4d3
nicobuton37 <nicobuton370@gmail.com>
2017-08-09 15:09:23 +0200

    chapter routing

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -10,9 +10,22 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AdvertController extends Controller
 {
+
+  public function viewAction($id)
+  {
+      return new Response("Affichage de l'annonce d'id : " . $id);
+  }
+
+  public function viewSlugAction($slug, $year, $format)
+  {
+    return new Response(
+      "On pourrait afficher l'annonce correspondant au slug '" . $slug . "', créér en " . $year . " et au format " . $format . "."
+    );
+  }
+
   public function indexAction()
   {
-    $content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.twig', array('name' => 'Mes couilles') );
+    $content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.twig', array('name' => 'Nico') );
 
     return new Response($content);
   }
